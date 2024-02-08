a8db897754043b2dc5cd156c8c026585061315ab
Your Name <Your Name>
2024-02-08 20:29:51 +0530

    server setup

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require("express")
+const cors = require("cors")
+require("dotenv").config({ path: "./.env" })
+const mongoose = require("mongoose")
 
+mongoose.connect(process.env.MONGO_URL)
 const app = express()
-
-app.listen(5000, console.log("SERVER RUNNING"))
+app.use(express.json())
+app.use(cors())
+app.use("/api/admin", require("./routes/todoRoute"))
+mongoose.connection.once("open", () => {
+    console.log("MONGO CONNECTED");
+    app.listen(5000, console.log("SERVER RUNNING"))
+})
